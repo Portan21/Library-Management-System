d0693819a0434181f2a16c1e65589dc046000acd
Portan21 <kyllepangilinan21@gmail.com>
2023-11-01 00:38:36 +0800

    Copied wireframe format of qr code generator

--- JavaScript/qrcodeGenerator(test).js
+++ JavaScript/qrcodeGenerator(test).js
@@ -1,6 +1,4 @@
 var qrText = document.getElementById('qrtext');
 
-function generateQR(){
-    qrcode.src="https://api.qrserver.com/v1/create-qr-code/?size=[250]x[250]&data=" + qrText.value;
-}
+qrcode.src="https://api.qrserver.com/v1/create-qr-code/?size=[250]x[250]&data=" + "INSERT NAME IN qrText.value.png";
 
