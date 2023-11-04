8f67e76950040fff0a726b35994f33996f9d6323
Portan21 <kyllepangilinan21@gmail.com>
2023-11-05 02:16:37 +0800

    Profile page now display data from database

--- PHP/profile.php
+++ PHP/profile.php
@@ -12,7 +12,7 @@ if(isset($_POST["login"])){
         if(password_verify($password, $row["password"])){
             $_SESSION["login"] = true;
             $_SESSION["accountID"] = $row["accountID"];
-            $_SESSION["idnumber"] = $row["idnumber"];
+            $_SESSION["IDnumber"] = $row["IDnumber"];
             $_SESSION["email"] = $row["email"];
             $_SESSION["first_name"] = $row["first_name"];
             $_SESSION["last_name"] = $row["last_name"];
@@ -44,10 +44,10 @@ if(isset($_POST["login"])){
                             <p class="overview">Overview</p>
                             <hr>
                         </div>
-                        <div class="row">
+                        <div class="row overview-row">
                             <div class="col-lg-4 static-info">
                                 <p class="static-text">Name</p>
-                                <p class="static-text">Username</p>
+                                <p class="static-text">Email</p>
                                 <p class="static-text">ID Number</p>
                                 <div class="col-lg-3">
                                 <a href='#'>
@@ -71,11 +71,13 @@ if(isset($_POST["login"])){
                                         </div>
 
                                         <!-- Borrow button with ID for styling -->
-                                        <div class="col-lg-3 d-flex justify-content-center">
-                                            <a download href="https://api.qrserver.com/v1/create-qr-code/?size=[250]x[250]&data=<?php echo $_SESSION["first_name"], " " ,
-                                            $_SESSION["last_name"]; ?>&download=1">
-                                            <input class="download-button format" type="submit" value="DOWNLOAD QR">
-                                            </a>
+                                        <div class="row d-flex justify-content-center">
+                                            <div class="col-lg-3 d-flex justify-content-center">
+                                                <a download href="https://api.qrserver.com/v1/create-qr-code/?size=[250]x[250]&data=<?php echo $_SESSION["first_name"], " " ,
+                                                $_SESSION["last_name"]; ?>&download=1">
+                                                <input class="download-button format" type="submit" value="DOWNLOAD QR">
+                                                </a>
+                                            </div>
                                         </div>
                                     
                                         <!-- Remove button with ID for styling 
@@ -84,10 +86,10 @@ if(isset($_POST["login"])){
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-8">
+                            <div class="col-lg-8 dynamic-info">
+                                <p><?php echo $_SESSION["first_name"], " " ,$_SESSION["last_name"]; ?></p>
                                 <p><?php echo $_SESSION["email"]; ?></p>
-                                <p>Username</p>
-                                <p>ID Number</p>
+                                <p><?php echo $_SESSION["accountID"]; ?></p>
                             </div>
                         </div>       
                     </div>
@@ -95,26 +97,38 @@ if(isset($_POST["login"])){
                 <div class="profile-form profile-form-right col-lg-6 border-0 shadow">
                     <div class="col-lg-11 profile-frame">
                         <div class="col-lg-12">
-                            <p class="overview">Overview</p>
+                            <p class="overview">Attendance History</p>
                             <hr>
                         </div>
-                        <div class="row">
-                            <div class="col-lg-2 static-info">
-                                <p class="static-text">Name</p>
-                                <p class="static-text">Username</p>
-                                <p class="static-text">ID Number</p>
-                            </div>
-                            <div class="col-lg-5">
-                                <p>Name</p>
-                                <p>Username</p>
-                                <p>ID Number</p>
-                            </div>
-                            <div class="col-lg-5">
-                                <p>Name</p>
-                                <p>Username</p>
-                                <p>ID Number</p>
-                            </div>
-                        </div>       
+                        <div class = "container attendance">
+                            <div class ="row">
+                            <table id="example" class="content-table" style="width:100%">
+                                <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Date</th>
+                                    <th>Time in</th>
+                                    <th>Time out</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                $result = mysqli_query($conn, "SELECT accountID,entry_time,exit_time FROM attendance");
+                                while($row = mysqli_fetch_assoc($result)){
+                                #$bookName = htmlspecialchars($row['book_name']);
+                                #$description = htmlspecialchars($row['description']);
+                                echo "<tr>
+                                    <td class='px-4 py-2 text-center border'>$row[accountID]</td>
+                                    <td class='px-4 py-2 text-center border'>$row[entry_time]</td>
+                                    <td class='px-4 py-2 text-center border'>$row[exit_time]</td>
+                                </tr>";
+                                
+                                }
+                                ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                     </div>
                 </div>
             </div>
@@ -123,22 +137,40 @@ if(isset($_POST["login"])){
                 <div class="profile-form profile-form-left col-lg-6 border-0 shadow">
                     <div class="col-lg-11 profile-frame">
                         <div class="col-lg-12">
-                            <p class="overview">Overview</p>
+                            <p class="overview">Borrowed Books</p>
                             <hr>
                         </div>
-                        <div class="row">
-                            <div class="col-lg-4 static-info">
-                                <p class="static-text">Name</p>
-                                <p class="static-text">Username</p>
-                                <p class="static-text">ID Number</p>
-                            </div>
-                            <div class="col-lg-8">
-                                <p>Name</p>
-                                <p>Username</p>
-                                <p>ID Number</p>
-                            </div>
-                        </div>       
+                        <div class = "container attendance">
+                            <div class ="row">
+                            <table id="example" class="content-table" style="width:100%">
+                                <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Date</th>
+                                    <th>Date of return</th>
+                                    <th>Title</th>
+                                    <th>Status</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                $result = mysqli_query($conn, "SELECT borrow_date,deadline,bookID,librarianID FROM borrowed_book"); #Add status and book title!!!!!!!! (librarianID for placeholder)
+                                while($row = mysqli_fetch_assoc($result)){
+                                #$bookName = htmlspecialchars($row['book_name']);
+                                #$description = htmlspecialchars($row['description']);
+                                echo "<tr>
+                                    <td class='px-4 py-2 text-center border'>$row[borrow_date]</td>
+                                    <td class='px-4 py-2 text-center border'>$row[deadline]</td>
+                                    <td class='px-4 py-2 text-center border'>$row[bookID]</td>
+                                    <td class='px-4 py-2 text-center border'>$row[librarianID]</td>
+                                </tr>";
+                                }
+                                ?>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
+                </div>
                 </div>
                 <div class="profile-form profile-form-right col-lg-6 border-0 shadow">
                     <div class="col-lg-11 profile-frame">
