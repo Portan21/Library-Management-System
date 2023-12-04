d85dc53482819fea531e449df520f2da60f45d77
marbrickaustria <austria.marbrick22@gmail.com>
2023-12-04 15:14:17 +0800

    request + auto delete over deadline

--- PHP/patron_landing.php
+++ PHP/patron_landing.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin-Landing</title>
+    <title>Landing</title>
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
