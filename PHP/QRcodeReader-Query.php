47264e99a00eb44bf50623b3bff3fb5379240bd3
Portan21 <kyllepangilinan21@gmail.com>
2023-12-04 11:20:43 +0800

    Updated to work on patrons' QR

--- PHP/QRcodeReader-Query.php
+++ PHP/QRcodeReader-Query.php
@@ -9,23 +9,43 @@ $conn = mysqli_connect("scribelibrary.mysql.database.azure.com", "scribeproject"
 
 $accID = $_POST['email'];
 
-// Be sure to set up your SQL $conn variable here
-$sql = "SELECT name, librarianID FROM lib_acc WHERE email = '$accID'";
-$result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_assoc($result);
-$librarianId = $row['librarianID'];
+if(substr($accID, -15) == "@adamson.edu.ph"){
+    $sql = "SELECT pt_name, patronID FROM patron_acc WHERE email = '$accID'";
+    $result2 = mysqli_query($conn, $sql);
+    $row2 = mysqli_fetch_assoc($result2);
+    $patronID = $row2['patronID'];
 
-$duplicateEmail = mysqli_query($conn, "SELECT lib_entry FROM lib_attendance WHERE librarianID = '$librarianId' AND lib_exit IS NULL");
-if(mysqli_num_rows($duplicateEmail) > 0){
-    $sql = "UPDATE lib_attendance SET lib_exit = '$currentDateTime' WHERE librarianID = $librarianId AND lib_exit IS NULL";
-    $result = mysqli_query($conn, $sql);
-    echo json_encode("<b>Name: $row[name] <br> Time Out: $currentDateTime");
+    $duplicateEmail = mysqli_query($conn, "SELECT pt_entry FROM patron_attendance WHERE patronID = '$patronID' AND pt_exit IS NULL");
+    if(mysqli_num_rows($duplicateEmail) > 0){
+        $sql = "UPDATE patron_attendance SET pt_exit = '$currentDateTime' WHERE patronID = $patronID AND pt_exit IS NULL";
+        $result = mysqli_query($conn, $sql);
+        echo json_encode("<b>Name: $row2[pt_name] <br> Time Out: $currentDateTime");
+    }
+    else {
+        $sql = "INSERT INTO patron_attendance(patronID, pt_entry) VALUES('$patronID','$currentDateTime')";
+        $result = mysqli_query($conn, $sql);
+        echo json_encode("<b>Name: $row2[pt_name] <br> Time In: $currentDateTime");
+    }
 }
-else {
-    $sql = "INSERT INTO lib_attendance(librarianID, lib_entry) VALUES('$librarianId','$currentDateTime')";
+else{
+    $sql = "SELECT name, librarianID FROM lib_acc WHERE email = '$accID'";
     $result = mysqli_query($conn, $sql);
-    echo json_encode("<b>Name: $row[name] <br> Time In: $currentDateTime");
+    $row = mysqli_fetch_assoc($result);
+    $librarianId = $row['librarianID'];
+
+    $duplicateEmail = mysqli_query($conn, "SELECT lib_entry FROM lib_attendance WHERE librarianID = '$librarianId' AND lib_exit IS NULL");
+    if(mysqli_num_rows($duplicateEmail) > 0){
+        $sql = "UPDATE lib_attendance SET lib_exit = '$currentDateTime' WHERE librarianID = $librarianId AND lib_exit IS NULL";
+        $result = mysqli_query($conn, $sql);
+        echo json_encode("<b>Name: $row[name] <br> Time Out: $currentDateTime");
+    }
+    else {
+        $sql = "INSERT INTO lib_attendance(librarianID, lib_entry) VALUES('$librarianId','$currentDateTime')";
+        $result = mysqli_query($conn, $sql);
+        echo json_encode("<b>Name: $row[name] <br> Time In: $currentDateTime");
+    }
 }
+
 
 // if ($row['lib_entry']) {
     
