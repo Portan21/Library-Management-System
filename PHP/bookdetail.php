3920a22f4f4a3eb442e5ae45800c4bc00b4c9990
marbrickaustria <austria.marbrick22@gmail.com>
2023-12-10 15:46:45 +0800

    add style

--- PHP/bookdetail.php
+++ PHP/bookdetail.php
@@ -70,6 +70,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book Details</title> 
+    <style>
+        .table-borderless td,
+        .table-borderless th {
+            border: 0;
+        }
+    </style>
     <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
 
 </head>
